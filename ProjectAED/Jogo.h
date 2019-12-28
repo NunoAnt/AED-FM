da1725c30f323623259eb95cbf7f0b23b18b40e0
Nuno Antunes <nunoantunes.cpm@gmail.com>
2019-12-28 15:18:59 +0000

    teste

--- ProjectAED/Jogo.h
+++ ProjectAED/Jogo.h
@@ -1,1 +1,16 @@
-#pragma once
+#include <stdio.h>
+#include <string.h>
+
+#define FICHEIRO "Jogos.dat"
+#define TRUE 1
+#define FALSE 0
+
+// Estrutura do Treinador
+typedef struct {
+	int id;
+	int idEquipa1;
+	int idEquipa2;
+	int dia, mes, ano;
+	int golosEquipa1;
+	int golosEquipa2;
+} Jogo;
